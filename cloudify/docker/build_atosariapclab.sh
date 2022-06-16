47a4f6fa4d0bfe244a3c302a677187ea3aea9669
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2022-06-16 08:56:05 +0000

    fix: build of Cloudify for PerMedCoE, and Keycloak version

--- cloudify/docker/build_atosariapclab.sh
+++ cloudify/docker/build_atosariapclab.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export IMAGE_NAME=croupier-permedcoe
-export TAG_NAME=latest
+export IMAGE_NAME=cloudify-permedcoe
+export TAG_NAME=6.2.0
 
 # Build the docker image
 docker build --rm=true -t $IMAGE_NAME:$TAG_NAME .
 
-docker image tag croupier-permedcoe:latest atosariapclab/croupier-permedcoe:latest
+docker image tag $IMAGE_NAME:$TAG_NAME atosariapclab/$IMAGE_NAME:$TAG_NAME
 docker login
-docker push atosariapclab/croupier-permedcoe:latest
+docker push atosariapclab/$IMAGE_NAME:$TAG_NAME
